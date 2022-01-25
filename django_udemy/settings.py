1892127c978aa5da7faa0b954407e1b21b1e2fc3
Евгений <volga52@bk.ru>
2022-01-25 12:36:36 +0300

    Lesson_4 add auths models

--- django_udemy/settings.py
+++ django_udemy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'authapp.TravelUser'
